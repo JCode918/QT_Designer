5855892a2f3f49e38fe4f4de9122cb6f2b042f93
JCode918 <Willard.Joseph26@gmail.com>
2020-06-16 23:34:33 -0400

    Pushing all changes to Repo

--- SimpleWindow/main.cpp
+++ SimpleWindow/main.cpp
@@ -1,15 +1,19 @@
 #include <QApplication>
 #include <qpushbutton.h>
+#include <QLabel>
 
 int main (int argc, char **argv)
 {
     QApplication app( argc, argv);
 
     QPushButton helloButton ("Hello World", 0);
+    QLabel WindowLabel("<center>Welcome to my first Qt program</center>");
+    WindowLabel.resize(400, 400);
     helloButton.resize(100, 30);
     helloButton.setWindowTitle("My First Qt Program");
     //app.setMainWidget (&helloButton);
     helloButton.show();
+    WindowLabel.show();
 
     return app.exec();
 }
